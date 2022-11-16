89e79835051310ca3d7dfe6e795f049dedfe9fe9
kodoklangit.1 <kodoklangit.1@gmail.com>
2022-11-16 23:54:26 +0700

    enhance

--- vpc/nat-gw.tf
+++ vpc/nat-gw.tf
@@ -1,13 +1,3 @@
-resource "aws_internet_gateway" "staging-igw" {
-  vpc_id = "${aws_vpc.staging-vpc.id}"
-
-  tags = {
-    Name    = "staging-igw"
-    Init = "terraform"
-    Recipe  = "${var.IAC_REPO_BRANCH}"
-  }
-}
-
 # EIP nat staging-private
 resource "aws_eip" "nat-staging-private-eip" {
   vpc = true

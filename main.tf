dc2e2c8ff5bcf2bcd38a2b4794e5695e9cd578f3
kodoklangit.1 <kodoklangit.1@gmail.com>
2022-11-16 22:22:11 +0700

    enhance

--- main.tf
+++ main.tf
@@ -2,18 +2,6 @@ module "vpc" {
   source = "./vpc"
 }
 
-module "iam" {
-  source = "./iam"
-}
-
-module "iam-role" {
-  source = "./iam/role"
-}
-
-module "keys" {
-  source = "./keys"
-}
-
 module "ec2" {
   source = "./ec2"
 }
